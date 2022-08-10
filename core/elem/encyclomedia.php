bc5df52d22589077eade27cca64de73cec941740
knowgistics-coding <63992663+knowgistics-coding@users.noreply.github.com>
2022-08-10 18:16:17 +0700

    Encyclopedia -> change Box style

--- core/elem/encyclomedia.php
+++ core/elem/encyclomedia.php
@@ -3,6 +3,44 @@
 function render_jp_encyclopedia_searchbox($bgImage)
 {
   echo '<style>
+  .encyclopedia-container {
+    position: relative;
+  }
+  .encyclopedia-container:before {
+    content: "";
+    display: block;
+    padding-top: calc(100% / 2);
+  }
+  .encyclopedia-container .encyclopedia-absolute {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .encyclopedia-absolute h2 {
+    color: #333;
+    font-family: "Noto Sans JP","Prompt";
+    font-size: 48px;
+    text-align: center;
+  }
+  .encyclopedia-content {
+    width: 640px;
+    max-width: 90%;
+  }
+  @media screen and (max-width: 768px){
+    .encyclopedia-container:before {
+      padding-top: calc(100%);
+    }
+    .encyclopedia-absolute h2 {
+      font-size: var(--font-l);
+      text-align: center;
+    }
+  }
   .search-wrap-body {
     border: solid 1px #333;
     display: flex;
@@ -26,20 +64,25 @@ function render_jp_encyclopedia_searchbox($bgImage)
     color: var(--hilight-color);
   }
   </style>';
-  echo '<div class="cover-contain-md" style="background-image:url(\''.$bgImage.'\')">
-    <div class="cover-meta" style="background-color:#faf7e8;padding: 1rem;">
-      <h2 style="color:#333;font-family:\'Noto Sans JP\',\'Prompt\';font-weight:bold;">สารานุกรมวัฒนธรรมญี่ปุ่น<br />日本文化百科事典</h2>
-      <form class="search-wrap-body" action="/encyclopedia/" method="post">
-        <input
-          class="form-control"
-          type="text"
-          name="search"
-          placeholder="ค้นหา"
-          required
-          oninvalid="this.setCustomValidity(\'กรุณากรอกคำค้นหา\')"
-        />
-        <button class="btn-submit"><i class="fas fa-search"></i></button>
-      </form>
+  echo '<div class="container" style="padding:0 1rem;">
+    <div class="encyclopedia-container" style="background-image:url(\''.$bgImage.'\')">
+      <div class="encyclopedia-absolute">
+        <div class="encyclopedia-content" style="background-color:#faf7e8;padding: 1rem;">
+          <h2 style="font-weight:bold;">สารานุกรมวัฒนธรรมญี่ปุ่น</h2>
+          <h2>日本文化百科事典</h2>
+          <form class="search-wrap-body" action="/encyclopedia/" method="post">
+            <input
+              class="form-control"
+              type="text"
+              name="search"
+              placeholder="ค้นหา"
+              required
+              oninvalid="this.setCustomValidity(\'กรุณากรอกคำค้นหา\')"
+            />
+            <button class="btn-submit"><i class="fas fa-search"></i></button>
+          </form>
+          </div>
+      </div>
     </div>
   </div>';
 }
