3a0fb44760ba92db0de77e6048b2a68fadcff020
knowgistics-coding <63992663+knowgistics-coding@users.noreply.github.com>
2022-10-29 11:22:14 +0700

    ajax_enqueuescripts check exists

--- core/plugin.php
+++ core/plugin.php
@@ -8,10 +8,12 @@ function get_complete_meta( $post_id, $meta_key ) {
   return false;
 }
 
-function ajax_enqueuescripts() {
-  wp_localize_script( 'ajaxloadpost', 'ajax_postajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+if( function_exists("ajax_enqueuescripts")==false ){
+  function ajax_enqueuescripts() {
+    wp_localize_script( 'ajaxloadpost', 'ajax_postajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+  }
+  add_action('wp_enqueue_scripts', "ajax_enqueuescripts");
 }
-add_action('wp_enqueue_scripts', "ajax_enqueuescripts");
 
 // Home Setting
 if(function_exists("phrain_theme_menu")==false){
